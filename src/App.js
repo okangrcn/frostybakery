8a0bd2e6401e5b52c6f0da971d912b988d8d223b
okan.gurcan <ogurcan@aselsan.com.tr>
2025-04-25 17:26:21 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { SepetProvider } from './SepetContext';
 import Header from './Header';
 import Banner from './Banner';
 import Urunler from './Urunler';
-import Hakkimizda from './Hakkimizda'
+import Hakkimizda from './Hakkimizda';
 import Footer from './Footer';
 import Sepet from './Sepet'; // Sepet bileşeni varsa ekleyebilirsin
 
